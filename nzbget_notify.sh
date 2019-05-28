1021a70646af323eae4dd4b33fc1ee9c642ce741
Jody <jodfie@gmail.com>
2019-05-27 22:10:06 -0400

    edit file path

--- nzbget_notify.sh
+++ nzbget_notify.sh
@@ -11,7 +11,7 @@ pip install pynzbget
 pip install chardet
 
 ##Get NZBNotify
-[[ ! -d /config/nzbnotify/.git ]] && (git clone git://github.com/caronc/nzb-notify.git /config/nzbnotify/ && chown -R 501:1000 /nzbnotify)
+[[ ! -d /config/nzbnotify/.git ]] && (git clone git://github.com/caronc/nzb-notify.git /config/nzbnotify/ && chown -R 501:1000 /config/nzbnotify)
 
 ###NZBNotify Script
 [[ ! -f /scripts/nzbnotify/notify.py ]] && (cp /config/nzbnotify/notify.py /scripts/nzbnotify/)
