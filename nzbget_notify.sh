729625ae6fd667bc7ae4af0fea6d844e4d9e75d3
Jody <jodfie@gmail.com>
2019-05-27 23:09:08 -0400

    add Notify Folder

--- nzbget_notify.sh
+++ nzbget_notify.sh
@@ -19,6 +19,9 @@ pip install chardet
 ###NZBNotify Script
 [[ ! -f /config/scripts/nzbnotify/Notify.py ]] && (cp /config/nzbnotify/Notify.py /config/scripts/nzbnotify/)
 
+###NZBNotify Folder
+[[ ! -f /config/scripts/nzbnotify/Notify ]] && (cp /config/nzbnotify/Notify /config/scripts/nzbnotify/Notify)
+
 # test if updates have been disabled
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
